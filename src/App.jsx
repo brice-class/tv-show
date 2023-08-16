c3c0c61d73bd24a10731fdd209b4a33fb569fc80
Brice Leblanc <d2_brice@hotmail.fr>
2023-08-16 16:58:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { TVShowAPI } from "./api/tv-show";
+import "./global.css";
+import s from "./style.module.css";
+import { BACKDROP_BASE_URL } from "./config";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+import { Logo } from "./components/Logo/Logo";
+import logo from "./assets/images/logo.png";
+import { TVShowListItem } from "./components/TVShowListItem/TVShowListItem";
+import { TVShowList } from "./components/TVShowList/TVShowList";
+import { SearchBar } from "./components/SeachBar/SearchBar";
+
+export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+  const [recommendationList, setRecommendationsList] = useState([]);
+
+  async function fetchPopulars() {
+    const populars = await TVShowAPI.fetchPopulars();
+    if (populars.length > 0) {
+      setCurrentTVShow(populars[0]);
+    }
+  }
+
+  async function fetchRecommendations(tvShowId) {
+    const recommendations = await TVShowAPI.fetchRecommendations(tvShowId);
+    if (recommendations.length > 0) {
+      setRecommendationsList(recommendations.slice(0, 10));
+    }
+  }
+
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  useEffect(() => {
+    if (currentTVShow) {
+      fetchRecommendations(currentTVShow.id);
+    }
+  }, [currentTVShow]);
+
+  function setCurrentTvShowFromRecommendation(tvShow) {
+    alert(JSON.stringify(tvShow));
+  }
+
+  async function searchTVShow(tvShowName) {
+    const searchResponse = await TVShowAPI.fetchByTitle(tvShowName);
+    if (searchResponse.length > 0) {
+      setCurrentTVShow(searchResponse[0]);
+    }
+  }
+  console.log("***", recommendationList);
+  return (
+    <div
+      className={s.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <Logo
+              image={logo}
+              title="Watowatch"
+              subtitle="Find a show you may like"
+            />
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <SearchBar onSubmit={searchTVShow}></SearchBar>
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_details}>
+        {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
+      </div>
+      <div className={s.recommended_shows}>
+        {recommendationList && recommendationList.length > 0 && (
+          <TVShowList
+            onClickItem={setCurrentTVShow}
+            tvShowList={recommendationList}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
